39fc5e27a0ce4dfe937b582479568e709d6997d6
Chris Vamvalis <vamvalis@csd.auth.gr>
2020-03-07 15:38:15 +0200

    Update to logic

--- ConsoleApp1/ApplicationLogic.cs
+++ ConsoleApp1/ApplicationLogic.cs
@@ -8,7 +8,8 @@ namespace ConsoleApp1
 {
     public static class ApplicationLogic
     {
-        public static void Logic(List<Event> mandatoryEvents, List<LessonWithMultipleTimes> lessonsWithMultipleTimes)
+        //roundTripWeight = the time to go to the university and back. (We assume that this will be done once a day).
+        public static void Logic(List<Event> mandatoryEvents, List<LessonWithMultipleTimes> lessonsWithMultipleTimes,double roundTripWeight)
         {
             List<Event> allEvents = new List<Event>();
             allEvents.AddRange(mandatoryEvents);
@@ -19,64 +20,141 @@ namespace ConsoleApp1
 
             var notFixed = allEvents.Where(x => x.isFixed = false); //all the not fixed events
 
-
+            List<TimeCalculate> times = new List<TimeCalculate>();
 
             foreach (var notFixedEvent in notFixed)
             {
                 //search in allnonfixed if this is the only one with that lessonId. (if it is then automatically add it to final list- make it fixed)
                 if(notFixed.Where(x=> x.Lesson == notFixedEvent.Lesson).ToList().Count == 1)
                 {
+                    //it may not be updated in the allEvents
                     notFixedEvent.isFixed = true; //we may need to mark it in another way
                 }
                 else
                 {
                     var sameDayEvents = allEvents.Where(x => x.Day == notFixedEvent.Day).ToList();
-                    //exclude itself from the search list
+
+                    double tempOffset = 0;
+                    sameDayEvents = sameDayEvents.OrderBy(x => x.startTime).ToList();
                     var thisEvent = sameDayEvents.Single(x => x.Id == notFixedEvent.Id);
-                    sameDayEvents.Remove(thisEvent);
 
-                    //we need to sum the before and after offset but be careful to not sum it with
-                    //an event from the same LessonWithMultipleTimes
-                    
-                    double offset = double.MaxValue;
-                    Event bestEvent;
-
-                    //find nearest and calculate offset
-                    foreach (var sameDayEvent in sameDayEvents)
+                    var time = new TimeCalculate
                     {
-                        if(sameDayEvent.Lesson == notFixedEvent.Lesson)
-                        {
-                            continue; //maybe we don't need to check this
-                        }
+                        Event = thisEvent
+                    };
 
-                        if (sameDayEvent.startTime < notFixedEvent.startTime)//to ypoxreotiko einai prin apo to ergastirio
+                    double xronospetamenos = 0;
+
+                    //find the total offset in that day. Begin from the first event of the day and sum the time to go.
+                    //be careful: when an event has the same Lesson as the one in notFixedEvent... We should ignore it
+                    //Also don't forget about dependencies
+                    for (int i = 0; i < sameDayEvents.Count; i++)
+                    {
+                        if(sameDayEvents[i] != thisEvent && sameDayEvents[i].Lesson == thisEvent.Lesson)
                         {
-                            var currOffset = (notFixedEvent.startTime - sameDayEvent.finishTime).TotalMinutes;
-                            if (currOffset < offset)
-                            {
-                                offset = currOffset;
-                                bestEvent = notFixedEvent;
-                                //we also need to track which "time" this was
-                            }
+                            //ignore it
                         }
                         else
                         {
-                            var currOffset = (a.startTime - time.finishTime).TotalMinutes;
-                            if (currOffset < offset)
+                            //find the difference between this and the next event
+                            int tempvalue = 1;
+                            while (i + tempvalue < sameDayEvents.Count && sameDayEvents[i + tempvalue].Lesson == notFixedEvent.Lesson)
                             {
-                                offset = currOffset;
-                                bestEvent = time;
+                                tempvalue++;
                             }
+
+                            if (i + tempvalue < sameDayEvents.Count)
+                            {
+                                //meaning that we found match
+                                var afterEvent = sameDayEvents[i + tempvalue];
+                                xronospetamenos += (afterEvent.startTime - notFixedEvent.finishTime).TotalMinutes;
+
+                                if (!afterEvent.isFixed) //&& samedayevents[i].isFixed
+                                {
+                                    //add dependency
+                                    time.AfterEventDependency = afterEvent;
+                                }
+                            }
+                            else
+                            {
+                                time.AfterEventDependency = null;
+                                //we don't add the tempOffset because it doesn't have anything after that
+
+                                //we couldn't find any match after this event, handle it
+                            }
+
+
                         }
                     }
+                    
+
+                    //int index = sameDayEvents.IndexOf(thisEvent);
+                    //int tempvalue = 1;
+                    //while(index - tempvalue >= 0 && sameDayEvents[index-tempvalue].Lesson == notFixedEvent.Lesson)
+                    //{
+                    //    tempvalue++;
+                    //}
+                    
+                    //if(index-tempvalue>=0 )//meaning that we didn't reach the top of the list
+                    //{
+                    //    var beforeEvent = sameDayEvents[index - tempvalue];
+                    //    tempOffset += (notFixedEvent.startTime - beforeEvent.finishTime).TotalMinutes;
+
+                    //    if(!beforeEvent.isFixed)
+                    //    {
+                    //        //add dependency
+                    //        time.BeforeEventDependency = beforeEvent;
+                    //    }
+
+                    //}
+                    //else //index-tempvalue==0
+                    //{
+                    //    time.BeforeEventDependency = null;
+                    //    //we don't add the tempOffset because it doesn't have anything before that
+                    //    //it was the first of the list, or all the above were from the same lesson, handle it
+                    //}
 
 
+                    //tempvalue = 1;
+                    //while(index + tempvalue < sameDayEvents.Count && sameDayEvents[index+tempvalue].Lesson == notFixedEvent.Lesson)
+                    //{
+                    //    tempvalue++;
+                    //}
 
+                    //if(index + tempvalue < sameDayEvents.Count)
+                    //{
+                    //    //meaning that we found match
+                    //    var afterEvent = sameDayEvents[index + tempvalue];
+                    //    tempOffset += (afterEvent.startTime - notFixedEvent.finishTime).TotalMinutes;
 
+                    //    if (!afterEvent.isFixed)
+                    //    {
+                    //        //add dependency
+                    //        time.AfterEventDependency= afterEvent;
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    time.AfterEventDependency = null;
+                    //    //we don't add the tempOffset because it doesn't have anything after that
+
+                    //    //we couldn't find any match after this event, handle it
+                    //}
+
+                    time.Time = tempOffset;
+                    times.Add(time);
                 }
             }
+
+            /* from times we have:
+                OptA -> 300min (No dependency)
+
+            
+
+            */
             //we need to make sure that for each LessonWithMultipleTimes there is only one event in the final calendar
         }
+        
 
     }
 }
---
+++ ConsoleApp1/TimeCalculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfApp1;
+
+namespace ConsoleApp1
+{
+    public class TimeCalculate
+    {
+        public Event Event { get; set; }
+
+        public double Time { get; set; }
+
+        public Event BeforeEventDependency { get; set; }
+
+        public Event AfterEventDependency { get; set; }
+
+    }
+}
