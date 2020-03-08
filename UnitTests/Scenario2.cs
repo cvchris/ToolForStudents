836bb54382eed464fb80bd70c1caca99236aef50
Chris Vamvalis <vamvalis@csd.auth.gr>
2020-03-08 21:05:09 +0200

    Update Scenario2.cs

--- UnitTests/Scenario2.cs
+++ UnitTests/Scenario2.cs
@@ -18,58 +18,59 @@ namespace UnitTests
 
 
             var mathima_a = new List<Event>();
-            mathima_a.Add(new Event
+            var m_a_a = new Event
             {
                 Id = 1,
                 IsFixed = false,
                 Day = DayOfWeek.Monday,
-                startTime = new DateTime(1, 1, 1, 13, 00, 00),
-                finishTime = new DateTime(1, 1, 1, 14, 00, 00)
-            });
-            mathima_a.Add(new Event
+            };
+            m_a_a.SetTime(13, 00, 14, 00);
+
+            mathima_a.Add(m_a_a);
+            var m_a_b = new Event
             {
                 Id = 2,
                 IsFixed = false,
                 Day = DayOfWeek.Monday,
-                startTime = new DateTime(1, 1, 1, 15, 00, 00),
-                finishTime = new DateTime(1, 1, 1, 16, 00, 00)
-            });
-            mathima_a.Add(new Event
+            };
+            m_a_b.SetTime(15, 00, 16, 00);
+            mathima_a.Add(m_a_b);
+            var m_a_c = new Event
             {
                 Id = 3,
                 IsFixed = false,
                 Day = DayOfWeek.Tuesday,
-                startTime = new DateTime(1, 1, 1, 15, 00, 00),
-                finishTime = new DateTime(1, 1, 1, 16, 00, 00)
-            });
+            };
+            m_a_c.SetTime(15, 00, 16, 00);
+            mathima_a.Add(m_a_c);
 
             var mathima_b_events = new List<Event>();
-            mathima_b_events.Add(new Event
+            var m_b_a = new Event
             {
                 Id = 4,
                 IsFixed = false,
                 Day = DayOfWeek.Wednesday,
-                startTime = new DateTime(1, 1, 1, 15, 00, 00),
-                finishTime = new DateTime(1, 1, 1, 16, 00, 00)
-            });
-            mathima_b_events.Add(new Event
+            };
+            m_b_a.SetTime(15, 00, 16, 00);
+            mathima_b_events.Add(m_b_a);
+            var m_b_b = new Event
             {
                 Id = 5,
                 IsFixed = false,
                 Day = DayOfWeek.Wednesday,
-                startTime = new DateTime(1, 1, 1, 19, 00, 00),
-                finishTime = new DateTime(1, 1, 1, 20, 00, 00)
+            };
+            m_b_b.SetTime(19, 00, 20, 00);
+            mathima_b_events.Add(m_b_b);
 
-            });
-
-            mandatoryEvents.Add(new Event
+            var mandatoryA = new Event
             {
                 Id = 6,
                 IsFixed = true,
                 Day = DayOfWeek.Wednesday,
-                startTime = new DateTime(1, 1, 1, 17, 00, 00),
-                finishTime = new DateTime(1, 1, 1, 18, 00, 00)
-            });
+            };
+            mandatoryA.SetTime(17, 00, 18, 00);
+
+            mandatoryEvents.Add(mandatoryA);
 
             var m_a = new LessonWithMultipleTimes(mathima_a);
             var m_b = new LessonWithMultipleTimes(mathima_b_events);
